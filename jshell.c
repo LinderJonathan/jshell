7a1414904c4160ee4631b95849162cfd8cf6cae3
LinderJonathan <linderjonathan3@gmail.com>
2025-12-13 00:34:12 +0100

    first attempt calling execvp

--- jshell.c
+++ jshell.c
@@ -1,27 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<stdbool.h>
-
 #include"jshell.h"
 
 int main()
 {
 	printf(
-		"##################################################\n"
-		"############### WELCOME TO J-SHELL ###############\n"
-		"##################################################\n"
+		"                __                             __       		\n"
+		" _      _____  / /________  ____ ___  ___     / /_____  		\n"
+		"| | /| / / _ \\/ / ___/ __ \\/ __ `__ \\/ _ \\   / __/ __ \\ 	\n"
+		"| |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  / /_/ /_/ / 		\n"
+		"|__/|__/\\___/_/\\___/\\____/_/_/_/_/_/\\___/   \\__/\\____/   \n"
+		"      / /    / ___// / / / ____/ /   / /                		\n"
+		" __  / /_____\\__ \\/ /_/ / __/ / /   / /                 		\n"
+		"/ /_/ /_____/__/ / __  / /___/ /___/ /___               		\n"
+		"\\____/     /____/_/ /_/_____/_____/_____/               		\n"
+		"\n"
 		"\n"
 	);
 
 	bool shellRunning = true;
 	while (shellRunning)
 	{
+		char path[MAX_PATH_SIZE];
 		char inputBuff[MAX_INPUT_SIZE];
-		scanf("%s", inputBuff);
-		printf("%c", inputBuff[0]);
-		shellRunning = false;
-	}
+		if (getcwd(path, sizeof(path)) != NULL)
+		{
+			printf("%s > ", path);
+			scanf("%s", inputBuff);
+			
+			// TODO: read inputBuff into a list of arguments
+			char *args[] = {"ls", "-l", NULL };
 
+			if (strcmp(inputBuff, "exit") == 0)
+			{	
+				// TODO: read argument array into execvp
+				printf("executing ls \n");
+				execvp(args[0], args);
+			}
+		}
+
+	}
 	return 0;
 }
--- jshell.h
+++ jshell.h
@@ -1,1 +1,8 @@
-#define MAX_INPUT_SIZE 256
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdbool.h>
+#include<string.h>
+
+#define MAX_INPUT_SIZE      256
+#define MAX_PATH_SIZE       256
